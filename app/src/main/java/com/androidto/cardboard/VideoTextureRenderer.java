74023ebb023391f369c55a7d2571e6d023d217e5
Marc Ashman <marc@bnotions.com>
2014-10-23 23:33:44 -0400

    video pane working

--- app/src/main/java/com/androidto/cardboard/VideoTextureRenderer.java
+++ app/src/main/java/com/androidto/cardboard/VideoTextureRenderer.java
@@ -2,7 +2,6 @@ package com.androidto.cardboard;
 
 import android.content.Context;
 import android.media.MediaPlayer;
-import android.util.Log;
 
 import com.google.vrtoolkit.cardboard.sensors.MagnetSensor;
 
@@ -10,13 +9,13 @@ import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 import rajawali.Object3D;
-import rajawali.lights.ALight;
 import rajawali.lights.DirectionalLight;
 import rajawali.materials.Material;
 import rajawali.materials.textures.ATexture;
 import rajawali.materials.textures.VideoTexture;
 import rajawali.math.vector.Vector3;
 import rajawali.primitives.Plane;
+import rajawali.scene.RajawaliScene;
 import rajawali.vr.RajawaliVRRenderer;
 
 
@@ -38,17 +37,18 @@ public class VideoTextureRenderer extends RajawaliVRRenderer implements MagnetSe
 
     public void initScene() {
         super.initScene();
-        ALight mLight = new DirectionalLight(0, 0, 1);
-        getCurrentScene().addLight(mLight);
 
-//        mMediaPlayer = new MediaPlayer();
         mMediaPlayer = MediaPlayer.create(getContext(), R.raw.robotchicken);
-        Material material = new Material();
-        material.setColor( new float[] { 1f, 1f, 1f, 1f });
-        material.setColorInfluence(0);
+        mMediaPlayer.setLooping(true);
+        //start and pause so the first frame is shown
+        mMediaPlayer.start();
+        mMediaPlayer.pause();
 
-        videoTexture = new VideoTexture("test", mMediaPlayer);
+        videoTexture = new VideoTexture("video", mMediaPlayer);
         videoTexture.setInfluence(1);
+
+        Material material = new Material();
+        material.setColorInfluence(0);
         try {
             material.addTexture(videoTexture);
         } catch (ATexture.TextureException e) {
@@ -56,25 +56,14 @@ public class VideoTextureRenderer extends RajawaliVRRenderer implements MagnetSe
             throw new RuntimeException();
         }
 
-        Object3D object = new Plane(2, 2, 1, 1);
-        object.setMaterial(material);
-        object.setPosition(0, 0, -5f);
-        object.rotateAround(new Vector3(0, 1, 0), 180);
-        //object.setLookAt(getCurrentCamera().getPosition());
-        getCurrentScene().addChild(object);
-        //getCurrentCamera().rotateAround(new Vector3(0, 1, 0), 270);
-        //object.setLookAt(getCurrentCamera().getPosition());
+        Object3D videoPane = new Plane(2, 2, 1, 1);
+        videoPane.setMaterial(material);
+        videoPane.setPosition(-5, 0, 0);
+        videoPane.rotateAround(new Vector3(0, 1, 0), 90);
 
-        //getCurrentCamera().setLookAt(object.getPosition());
-
-
-        
-
-
-        mMediaPlayer.setLooping(true);
-        mMediaPlayer.seekTo(5000);
-        mMediaPlayer.start();
-        mMediaPlayer.pause();
+        RajawaliScene scene = getCurrentScene();
+        scene.addLight(new DirectionalLight(0, 0, 1));
+        scene.addChild(videoPane);
     }
 
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
@@ -93,24 +82,19 @@ public class VideoTextureRenderer extends RajawaliVRRenderer implements MagnetSe
     public void onVisibilityChanged(boolean visible) {
         super.onVisibilityChanged(visible);
 
-//        if (!visible) {
-//            if (mMediaPlayer != null) {
-//                mMediaPlayer.pause();
-//            }
-//        } else if (mMediaPlayer != null) {
-//            mMediaPlayer.start();
-//        }
+        if (!visible) {
+            if (mMediaPlayer != null) {
+                mMediaPlayer.pause();
+            }
+        } else if (mMediaPlayer != null) {
+            mMediaPlayer.start();
+        }
     }
 
     @Override
     public void onDrawFrame(GL10 glUnused) {
         super.onDrawFrame(glUnused);
         videoTexture.update();
-
-        if (getCurrentCamera() == null || getCurrentCamera().getLookAt() == null) {
-            return;
-        }
-        Log.w("look at", getCurrentCamera().getLookAt().toString());
     }
 
     @Override
