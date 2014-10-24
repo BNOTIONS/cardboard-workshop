a09ece7f43f83d9783df26767b7265d63a52c7a5
Marc Ashman <marc@bnotions.com>
2014-10-24 13:52:02 -0400

    API 19 was being used in API 16 context

--- rajawali-vr/src/main/java/rajawali/vr/RajawaliVRActivity.java
+++ rajawali-vr/src/main/java/rajawali/vr/RajawaliVRActivity.java
@@ -1,5 +1,6 @@
 package rajawali.vr;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
@@ -13,12 +14,15 @@ public class RajawaliVRActivity extends RajawaliActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		mSurfaceView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-	            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-	            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-	            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-	            | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-	            | View.SYSTEM_UI_FLAG_IMMERSIVE);
+        int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                | View.SYSTEM_UI_FLAG_FULLSCREEN; // hide status bar
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            flags |= View.SYSTEM_UI_FLAG_IMMERSIVE;
+        }
+		mSurfaceView.setSystemUiVisibility(flags);
 		mHeadTracker = new HeadTracker(this);
 	}
 	
